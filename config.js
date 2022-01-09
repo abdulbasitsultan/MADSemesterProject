a216217fde2de0fac0e78ba99c8f73d76773d810
Abdul Basit Sultan <abdulbasitsultan36@gmail.com>
2022-01-09 16:34:16 +0500

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDNr_gy8XqfJdr_3IP8C6a7RTbY7tzix80",
+    authDomain: "myfoodapp-9f010.firebaseapp.com",
+    projectId: "myfoodapp-9f010",
+    storageBucket: "myfoodapp-9f010.appspot.com",
+    messagingSenderId: "73174928791",
+    appId: "1:73174928791:web:25cfc2953316e14aca88d2",
+    measurementId: "G-6NNVM1HH3H"
+  };
